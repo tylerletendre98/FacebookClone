8111dfa4fb38066cdefa7bd63ad52e6e05b252e9
T U <tylerletendre98@gmail.com>
2021-11-04 11:32:23 -0400

    added sidebar file and all of the icons needed for the sidebar

---
+++ frontend/src/components/sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './sidebar.css'
+import SidebarRow from './SidebarRow'
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital'
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags'
+import PeopleIcon from '@material-ui/icons/People'
+import ChatIcon from '@material-ui/icons/Chat'
+import StoreFrontIcon from '@material-ui/icons/Storefront'
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
+import  ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined'
+import {useStateValue} from '../StateProvider'
+
+const Sidebar=()=>{
+    return(
+        <div>
+            <h1>this is sidebar</h1>
+        </div>
+    )
+}
+
+export default Sidebar;
