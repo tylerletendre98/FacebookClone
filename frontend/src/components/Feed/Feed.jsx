a6c7d5905b016ab33ae54fde4433c3b7bd7b18db
T U <tylerletendre98@gmail.com>
2021-11-12 11:41:41 -0500

    adding in styling to the message sender file and some functional code for later

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MessageSender from '../MessageSender/MessageSender'
 import StoryReel from '../StoryReel/StoryReel'
 import './feed.css'
 
@@ -6,6 +7,7 @@ export default function Feed() {
     return (
         <div className="feed">
             <StoryReel/>
+            <MessageSender/>
         </div>
     )
 }
--- frontend/src/components/MessageSender/MessageSender.jsx
+++ frontend/src/components/MessageSender/MessageSender.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Avatar, Input } from '@material-ui/core'
+import React, { useState } from 'react'
+import './messageSender.css'
 
 function MessageSender() {
+    const [input,setInput] = useState('')
+    const [image,setImage] = useState(null)
+
+    const handleChange = (e)=>{
+        if(e.target.files[0]){
+            setImage(e.target.files[0])
+        }
+    }
+
+    const handleSubmit =()=>{
+        console.log('Submitting')
+    }
     return (
         <div className="messageSender">
-            
+            <div className='messageSender-top'>
+                <Avatar src='https://tse3.mm.bing.net/th?id=OIP.Zdz-68DDO_vmcKRoV27V4AHaGq&pid=Api&P=0&w=205&h=186'/>
+                <form>
+                    <input 
+                    type="text" 
+                    className='messageSender-input'
+                    placeholder='Whats on your mind'
+                    value={input}
+                    onChange={(e)=>{setInput(e.target.value)}} />
+                    <Input 
+                    type="file"
+                    className='messageSender-fileSelector'
+                    onChange={handleChange}/>
+                    <button onClick={handleSubmit} type='submit'> Hidden Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
