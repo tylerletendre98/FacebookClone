f91b6b7cf5d93ba8ae7d183561e4fe19036fb76c
T U <tylerletendre98@gmail.com>
2021-11-17 13:04:47 -0500

    added in post markup going to work on styling

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,7 @@
 import { Avatar } from '@material-ui/core'
+import { ThumbUp, ChatBubbleOutline, NearMe, AccountCircle, ExpandMore} from '@material-ui/icons'
 import React from 'react'
+import './post.css'
 
 function Post({profilePic, message, userName, timeStamp, imgName}) {
     return (
@@ -8,6 +10,22 @@ function Post({profilePic, message, userName, timeStamp, imgName}) {
             <div className='post-topInfo'>
                 <h3>{userName}</h3>
                 <p>{new Date(parseInt(timeStamp)).toUTCString()}</p>
+                <div className="post-options">
+                    <ThumbUp/>
+                    <p>Like</p>
+                </div>
+                <div className="post-option">
+                    <ChatBubbleOutline/>
+                    <p>Comment</p>
+                </div>
+                <div className="post-option">
+                    <NearMe/>
+                    <p>Share</p>
+                </div>
+                <div className="post-option">
+                    <AccountCircle/>
+                    <ExpandMore/>
+                </div>
             </div>
         </div>
     )
